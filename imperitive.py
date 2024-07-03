319e4d79753bc5b9c6d4696c7b10771c224cb04e
Ojas Srivastava <ojas1807@gmail.com>
2024-07-03 04:43:56 +0000

    initial

---
+++ imperitive.py
@@ -0,0 +1,20 @@
+# IMPERITIVE PROGRAMMING is probably the easiest of all programming.
+# Think of your programming language as a REALLY BIG SMART calculator
+# That's all it is.
+
+print("Hello world!") # Do this
+32 + 12               # Then do this
+x = 2 + 2             # Then this
+print(x)              # Finally, this
+
+
+# Basically running commands in sequence. Pretty easy, right?
+# It's like a cooking recipe. You just give simple instructions and stuff happens.
+
+import matplotlib.pyplot as plt
+
+xPoints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+yPoints = [12, 23, 10, 9, 5, 23, 12, 6, 1, 18]
+
+plt.plot(xPoints, yPoints)
+plt.show()
